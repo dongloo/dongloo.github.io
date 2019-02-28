157f50d5047dd542bd8d63e0063dac0e357fdc62
Minh Ki Dong <minhkidong@gmail.com>
2019-02-28 12:04:46 +0700

    11
    22

--- check-chu/js/index.js
+++ check-chu/js/index.js
@@ -81,3 +81,155 @@
 			$(this).parents(".txt_check_item").find(".txt_taidam").removeClass("active");
 	    }
 	});
+
+//--DOCUMENT READY FUNCTION BEGIN
+jQuery(document).ready(function () {
+  	var availableTags = [
+		{
+			value: "Nơi",
+			id: "1"
+		},
+		{
+			value: "Trắng",
+			id: "2"
+		},
+		{
+			value: "Nước",
+			id: "3"
+		},
+		{
+			value: "Cá",
+			id: "4"
+		},
+		{
+			value: "Bố",
+			id: "5"
+		},
+		{
+			value: "Ông nội",
+			id: "6"
+		},
+		{
+			value: "Nhà",
+			id: "7"
+		},
+		{
+			value: "Trồng",
+			id: "8"
+		},
+		{
+			value: "Cây",
+			id: "9"
+		},
+		{
+			value: "Tết",
+			id: "10"
+		},
+		{
+			value: "Thịt",
+			id: "11"
+		},
+		{
+			value: "Xanh",
+			id: "12"
+		},
+		{
+			value: "Của",
+			id: "13"
+		},
+		{
+			value: "Ngày",
+			id: "14"
+		},
+		{
+			value: "Lợn",
+			id: "15"
+		},
+		{
+			value: "Tròn",
+			id: "16"
+		},
+		{
+			value: "Uống",
+			id: "17"
+		},
+		{
+			value: "Ném",
+			id: "18"
+		},
+		{
+			value: "Nương",
+			id: "19"
+		},
+		{
+			value: "Gà",
+			id: "20"
+		},
+		{
+			value: "Nấu",
+			id: "21"
+		},
+		{
+			value: "Cao",
+			id: "22"
+		},
+		{
+			value: "(con) Trâu",
+			id: "23"
+		},
+		{
+			value: "Tưới",
+			id: "24"
+		},
+		{
+			value: "Nhỏ",
+			id: "25"
+		},
+		{
+			value: "Núi",
+			id: "26"
+		},
+		{
+			value: "Lá",
+			id: "27"
+		},
+		{
+			value: "Lạnh",
+			id: "28"
+		},
+		{
+			value: "Chữ",
+			id: "29"
+		},
+		{
+			value: "Mẹ",
+			id: "30"
+		}      	
+    ];
+    jQuery(document).find("#browsers_txt_taidam").autocomplete({
+    	source: availableTags,
+    	select: function( event, ui ) {
+    		var value = ui.item.value;
+    		var id = ui.item.id;
+    		jQuery(document).find('.txt_check_item').removeClass("active");
+    		jQuery(document).find('.txt_check_item[data-id="'+id+'"]').addClass("active");
+      	}
+    });
+    jQuery(document).on('click', '.txt_clear_input', function(){
+        jQuery(document).find("#browsers_txt_taidam").val("");
+        jQuery(document).find('.txt_check_item').removeClass("active");
+    });
+});
+//--DOCUMENT READY FUNCTION END
+
+//--WINDOW LOADED FUNCTION BEGIN
+jQuery(window).bind("load", function () {
+    
+});
+//--WINDOW LOADED FUNCTION END
+
+//--WINDOW RESIZE FUNCTION BEGIN
+jQuery(window).resize(function () {
+    
+});
+//--WINDOW RESIZE FUNCTION END	
